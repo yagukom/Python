9781c2d5edba466b464b1e85f65c83a26b958c8f
炸天帮团员 <49666040+yagukom@users.noreply.github.com>
2021-01-11 09:28:36 +0800

    Update 递归函数实现反向打印字符.py

--- 递归函数实现反向打印字符.py
+++ 递归函数实现反向打印字符.py
@@ -1,7 +1,7 @@
 #2021-1-11 9:22
 #题目来自《自学是门手艺--李笑来》2.4.4递归函数 编程题1165
 #递归函数实现反向打印字符
-#昨天晚上写的是用for函数实现反向打印字符
+#昨天晚上写的是用for循环实现反向打印字符.py
 def StringReverseCoreFunc(string,length,ResultString):#字符串反向打印的核心递归函数
   if length==0:
     #result.append(string[length])此行多余，因为会重复输出最后一个字符
