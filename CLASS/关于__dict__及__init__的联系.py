21314df161bdc0e0b5d3a675f21145568c49c924
炸天帮团员 <49666040+yagukom@users.noreply.github.com>
2021-01-21 13:46:30 +0800

    Update 关于__dict__及__init__的联系.py

--- CLASS/关于__dict__及__init__的联系.py
+++ CLASS/关于__dict__及__init__的联系.py
@@ -1,3 +1,4 @@
+#以下内容来自《自学是门手艺--李笑来》3.2.2类Python的实现 第265题
 class Person:
     def __init__(self, id):
         self.id = id
@@ -5,7 +6,11 @@ class Person:
 obama = Person(100)
 
 obama.__dict__["age"] = 49
-
 print(obama.age + len(obama.__dict__))
 help(obama)
-obama.__dict__#可以看出，这东西变成了一个字典
+print(obama.__dict__)
+#可以看出，这东西变成了一个字典
+print(obama.__init__)
+obama.__dict__["id"] = 8
+print(obama.__dict__)
+#可以看出，我们可以通过字典的方式从外部更新__init__的self.xxx变量
